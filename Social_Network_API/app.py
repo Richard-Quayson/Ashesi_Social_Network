ee757f183addda52ac083f0f9695985b2cd9a18a
Richard Quayson <quaysonrichard23@gmail.com>
2023-04-12 16:46:52 +0000

    first commit

---
+++ Social_Network_API/app.py
@@ -0,0 +1,293 @@
+import json
+from flask import Flask, jsonify, request
+
+# import helper methods and other essential data
+from helper import (
+    FIRST_YEAR_GROUP, USERS_COLLECTION, POSTS_COLLECTION,
+    
+    valid_request_body, valid_student_id, valid_student_info,
+    valid_name, valid_dob, valid_major, valid_email, valid_post
+    
+)
+
+# creating flask app
+social_network = Flask(__name__)
+
+
+# _______________________________________________________________________________________________
+# REGISTER A STUDENT AS A USER
+@social_network.route("/users/profile/create/", methods=["POST"])
+def register_user():
+    
+    # validate student details in request
+    unique_keys = ["student_id", "email"]
+    result = valid_student_info(request, unique_keys)
+    if type(result) == tuple:
+        return result
+    
+    # write student's data into database
+    student_info = result["data"]
+    USERS_COLLECTION.document(student_info["student_id"]).set(student_info)
+    
+    return jsonify(student_info), 201
+
+
+# ______________________________________________________________________________________________
+# EDIT STUDENT PROFILE
+@social_network.route("/users/profile/edit/<student_id>/", methods=["PATCH"])
+def edit_profile(student_id):
+    
+    # ensure that the student_id specified is valid
+    valid_id = valid_student_id(student_id)
+    # if not syntactically correct, return appropriate message
+    if not valid_id:
+        return jsonify({"error": "Student id is not valid!"}), 400
+    # else if syntactically correct, ensure the year group is valid
+    elif int(valid_id["year_group"]) < FIRST_YEAR_GROUP:
+        return jsonify({"error": "Student year group is invalid!"}), 400
+
+    # ensure that the request body is not empty
+    if not valid_request_body(request):
+        return jsonify({"error": "Student update information missing!"}), 404
+    
+    # get request data
+    update_info = json.loads(request.data)
+    
+    # ensure that specified data does not contain student_id,
+    # name and email since they cannot be changed
+    
+    if "student_id" in update_info:
+        return jsonify({"error": "Student id cannot be changed!"}), 403
+    
+    if "firstname" in update_info or "lastname" in update_info:
+        return jsonify({"error": "Name cannot be changed!"}), 403
+    
+    if "email" in update_info:
+        return jsonify({"error": "Email cannot be changed!"}), 403
+    
+    if "dob" in update_info:
+        return jsonify({"error": "Date of birth cannot be changed"}), 403
+    
+    # ensure that provided update information are valid
+    
+    if "major" in update_info:
+        if not valid_major(update_info["major"]):
+            return jsonify({"error": "Major specified is not offered in Ashesi!"}), 400
+        
+    if "campus_resident" in update_info:
+        if bool(update_info["campus_resident"]) != True or bool(update_info["campus_resident"]) != False:
+            return jsonify({"error": "Campus resident must be a boolean!"})
+        
+    # get student with the specified id
+    student_data = USERS_COLLECTION.document(student_id).get().to_dict()
+    
+    # update specified info
+    for key in update_info:
+        student_data[key] = update_info[key]
+        
+    # write updated data back into database
+    USERS_COLLECTION.document(student_id).set(student_data)
+    
+    return jsonify(student_data)
+
+
+# _______________________________________________________________________________________________
+# VIEW STUDENT PROFILE
+@social_network.route("/users/profile/view/", methods=["GET"])
+def get_student_profile():
+    """uses all specified arguments (attributes of student) parsed for filtering
+    matching students and returns the result. If no attribute is parsed, it retrieves
+    all students. If any exception occur, it returns an appropriate message of the exception
+    * Attributes being used for filtering are: 
+    - student_id    - firstname     - lastname      - email         - dob
+    - year_group    - campus_resident   - best_food     - best_movie
+
+    Returns:
+        dict: JSON representation of the list of students (dict) that match filter attributes
+    """
+    
+    # dict to store all keys and values for filter
+    filter_dict = dict()
+    
+    # stores resulting list from filtering
+    result_list = list()
+    final_result_list = list()
+    
+    # get all attributes specified in the request args
+    if request.args.get("student_id"):
+        filter_dict["student_id"] = request.args.get("student_id")
+        
+    if request.args.get("firstname"):
+        filter_dict["firstname"] = request.args.get("firstname")
+        
+    if request.args.get("lastname"):
+        filter_dict["lastname"] = request.args.get("lastname")
+        
+    if request.args.get("email"):
+        filter_dict["email"] = request.args.get("email")
+        
+    if request.args.get("dob"):
+        filter_dict["dob"] = request.args.get("dob")
+        
+    if request.args.get("year_group"):
+        filter_dict["year_group"] = request.args.get("year_group")
+        
+    if request.args.get("campus_resident"):
+        filter_dict["campus_resident"] = request.args.get("campus_resident")
+        
+    if request.args.get("major"):
+        filter_dict["major"] = request.args.get("major")
+        
+    if request.args.get("best_food"):
+        filter_dict["best_food"] = request.args.get("best_food")
+
+    # read the students file
+    data = USERS_COLLECTION.get()
+    students_data = list()
+    for student in data:
+        students_data.append(student.to_dict())
+        
+    if not students_data:
+        return jsonify({"message": "No student has been registered!"}), 404
+    
+    # if no argument is parsed, retrieve all users
+    if not filter_dict:
+        return jsonify(students_data)
+        
+    for key in filter_dict.keys():
+
+        # ensure that the value of key is valid
+        if key == "student_id":
+            if not valid_student_id(filter_dict[key]):
+                return jsonify({"message": "Student ID is not valid."}), 400
+
+        elif key == "firstname":
+            if not valid_name(filter_dict[key]):
+                return jsonify({"message": "Firstname must be a string."}), 400
+            
+        elif key == "lastname":
+            if not valid_name(filter_dict[key]):
+                return jsonify({"message": "Lastname must be a string."}), 400
+        
+        elif key == "email":
+            if not valid_email(filter_dict[key]):
+                return jsonify({"message": "Email must be a valid Ashesi email address."}), 400
+        
+        elif key == "major":
+            if not valid_major(filter_dict[key]):
+                return jsonify({"message": "Major not offered in Ashesi."}), 404
+            
+        elif key == "dob":
+            if not valid_dob(filter_dict[key]):
+                return jsonify({"message": "Date of birth is not valid."}), 400
+            
+        elif key == "campus_resident":
+            if filter_dict[key].lower() == "true":
+                filter_dict[key] = True
+            elif filter_dict[key].lower() == "false":
+                filter_dict[key] = False
+            else:
+                return jsonify({"message": "Invalid value for campus_resident attribute!"}), 400
+            
+        elif key == "year_group":
+            if int(filter_dict["year_group"]) < FIRST_YEAR_GROUP:
+                return jsonify({"message": "Student year group is invalid."})
+            
+        # empty the result list before new filter is applied
+        result_list.clear()
+            
+        # get all user with specified key
+        for student in students_data:
+            # since year group isn't being stored, handle it differently
+            if key == "year_group":
+                # get year group of student from student's student_id
+                student_id_details = valid_student_id(student.get("student_id"))
+                year_group = student_id_details["year_group"]
+                if year_group == filter_dict[key]:
+                    result_list.append(student)
+            else:
+                if type(filter_dict[key]) == str:
+                    if student.get(key).lower() == filter_dict[key].lower() or student.get(key).lower().startswith(filter_dict[key].lower()):
+                        result_list.append(student)
+                else:
+                    if student.get(key) == filter_dict[key]:
+                        result_list.append(student)
+        
+        # update the data being returned
+        final_result_list = result_list[:]
+        
+        # update the data being used for filtering
+        students_data = final_result_list
+                
+    # ensure that the result list is not empty
+    if not final_result_list:
+        return jsonify({"message": "No student found with the provided details"}), 404
+            
+    return jsonify(final_result_list)
+
+
+# _________________________________________________________________________________________________
+# CREATE POST
+@social_network.route("/users/posts/create/", methods=["POST"])
+def create_post():
+    
+    
+    # ensure that post_data is valid
+    result = valid_post(request)
+    if type(result) == tuple:
+        return result
+        
+    # write data to collection
+    post_data = result["data"]
+
+    if post_data["post_image"]:
+        post_image = request.files()
+        
+        
+    POSTS_COLLECTION.document().set(post_data)
+    
+    return jsonify(post_data)
+
+
+
+# ________________________________________________________________________________________________
+# VIEW FEED
+@social_network.route("/users/posts/feed/", methods=["GET"])
+def retrieve_feed():
+    
+    # attributes to be used include:
+    # name, either first or last
+    # student_id, year group
+    # and description of feed / post
+    
+    # idea: get the value the user entered in the text field
+    value = request.args.get("value")
+    
+    # determine the type of the value
+    # whether it's firstname, lastname, student_id,
+    # year_group or description
+    
+    if valid_student_id(value):
+        key = "student_id"
+    elif valid_name(value):
+        key = "name"
+    elif len(value) == 4 and value.isdigit():
+        key = "year_group"
+    elif valid_email(value):
+        key = "email"
+    else:
+        key = "description"
+    
+    # if user attribute detected, get corresponding email and filter post
+    if key == "student_id" or key == "year_group":
+        pass
+    # else if description detected, get corresponding posts with said description
+    elif key == "email" or key == "description":
+        pass
+    # else, return appropriate response of unrecognisable attribute
+    else:
+        return jsonify({"error": "Unrecognisable attribute!"}), 404
+    pass
+    
+if __name__=='__main__':
+    social_network.run(debug=True)
---
+++ Social_Network_API/helper.py
@@ -0,0 +1,364 @@
+import json
+from datetime import datetime
+from flask import jsonify
+from firebase_admin import credentials, firestore, initialize_app
+
+
+# initialise Firestore database
+cred = credentials.Certificate("key.json")
+app = initialize_app(cred)
+database = firestore.client()
+
+# collection reference
+USERS_COLLECTION = database.collection("users")
+POSTS_COLLECTION = database.collection("posts")
+
+# first year group in Ashesi
+FIRST_YEAR_GROUP = 2002
+
+# valid Ashesi majors
+ASHESI_MAJORS = [
+    "BA", "CS", "MIS", "CE", "ME", "EE", "MECHATRONICS"
+]
+
+# expected student attributes
+STUDENT_ATTRIBUTES = [
+    "student_id", "firstname", "lastname", "email",
+    "dob", "major", "campus_resident", "best_food", "best_movie"
+]
+
+POST_ATTRIBUTES  =  [
+    "email", "description",
+]
+
+
+def valid_request_body(request):
+    """ensures that the request body is valid (not empty)
+
+    Args:
+        request (tuple): the request being sent to the API
+
+    Returns:
+        JSON: a boolean of whether or not the request is valid
+    """
+    
+    if not request.data:
+        return False    
+    return True
+
+
+def valid_keys(object_data, expected_keys):
+    """ensures that all required keys for specified object are present
+
+    Args:
+        object_data (dict): JSON representation of a object's information
+
+    Returns:
+        dict: a dictionary of boolean and or list of messages from validation
+    """
+    
+    result = {"is_valid": False}            # result from validation
+    result_message = []                     # message from validation
+    num_correct_keys = 0                    # variable to keep track of the number of matching keys
+    data_keys = object_data.keys()
+    
+    for key in expected_keys:
+        if key in data_keys:
+            num_correct_keys += 1
+        else:
+            result_message.append(f"{key.capitalize()} is required")
+    
+    if num_correct_keys == len(expected_keys):
+        
+        # if more keys are persent, flag the presence of extra keys
+        if len(object_data) > len(expected_keys):
+            result["message"] = f"Extra attributes identified. Expected keys are: {expected_keys}"
+        else:
+            result["is_valid"] = True
+    else:
+        result["message"] = result_message
+    
+    return result
+
+
+def unique_keys(key_list, data, object_data):
+    """ensures that all values corresponding to unique keys in the provided 
+    object_data is unique (does not already exist in the database)
+
+    Args:
+        key_list (list): list of unique keys
+        dictionary_list (list of dict): a list of object information (dict) in db
+        object_data (dict): a dictionary containing the object's information
+
+    Returns:
+        dict: a dictionary containing the result from unique test
+    """
+    
+    result = dict()
+    for key in key_list:
+        for voter in data:        
+            if object_data[key] == voter[key]:  
+                # account for uniqueness in update request, where student_id
+                # and voter["student_id"] != object_data["student_id"]:
+                result[key] = key + " already exists!"
+    
+    return result
+
+
+def valid_student_id(student_id):
+    """ensures that a given student_id is valid
+    - a student ID is valid if it's eight characters long and numeric
+
+    Args:
+        student_id (str): a student's ID
+
+    Returns:
+        dict: a dictionary containing user_id (the first four values of a student id)
+        and year_group (the year group of the student)
+    """
+    
+    # ensure that the student id is of length 8
+    if(len(student_id)) != 8:
+        return False
+    
+    # ensure that the student id is numeric
+    if not student_id.isnumeric():
+        return False
+    
+    user_id = student_id[:4]
+    year_group = student_id[4:]
+    
+    return {"user_id": user_id, "year_group": year_group} 
+
+
+def valid_name(name):
+    """determines whether or not a given name is valid (i.e. is a string)
+
+    Args:
+        name (string): the name to be verified
+
+    Returns:
+        bool: whether or not the name is valid
+    """
+       
+    if not str("firstname").isalpha():
+        return False
+    return True
+
+
+def valid_email(email):
+    """determines whether or not a given email is a valid ashesi email
+
+    Args:
+        email (string): the email of the student
+
+    Returns:
+        bool: whether or not the email is valid
+    """
+    
+    if not email.endswith("@ashesi.edu.gh"):
+        return False
+    return True
+
+
+def valid_dob(dob):
+    return True
+
+
+def valid_major(major):
+    """determines whether or not a given student major is a valid
+    major being offered in Ashesi University
+
+    Args:
+        major (str): the major of the student
+
+    Returns:
+        bool: whether or not the major is valid
+    """
+    
+    if major.upper() in ASHESI_MAJORS:
+        return True
+    return False
+
+def valid_image(filename):
+    """determines whether or not a given filename is an image
+
+    Args:
+        filename (str): the name of the file
+
+    Returns:
+        bool: whether or not the file is an image
+    """
+    
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+        return True
+    return False
+
+
+def valid_student_info(request, unique_keys_list):
+    """ensures that a student's request data is valid
+    i.e. contains all necessary keys; contains unique values for
+    unique keys; student_id, firstname, lastname and email are syntactically valid
+
+    Args:
+        request (tuple): request from client
+        unique_keys (list): a list of keys that should be unique
+
+    Returns:
+        dict: dictionary containing the status of the validation 
+        and a JSON representation of the voter's info from the request or
+        appropriate message if an exception occurred
+    """
+    
+    # ensure that the request body is not empty
+    if valid_request_body(request) == False:
+        return jsonify({"errro": "Student details missing!"}), 404
+    
+    # get request body
+    student_info = json.loads(request.data)
+    
+    # ensure that the student's info contains all required attributes
+    attributes = valid_keys(student_info, STUDENT_ATTRIBUTES)
+    if not attributes["is_valid"]:
+        return jsonify(attributes["message"]), 400
+    
+    # ensure that the student's id is syntactically correct
+    valid_id = valid_student_id(student_info["student_id"])
+    # if not syntactically correct, return appropriate message
+    if not valid_id:
+        return jsonify({"error": f"Student id ({student_info['student_id']}) is not valid!"}), 400
+    # else if syntactically correct, ensure the year group is valid
+    elif int(valid_id["year_group"]) < FIRST_YEAR_GROUP:
+        return jsonify({"error": f"Student year group ({valid_id['year_group']}) is invalid!"}), 400
+    
+    # ensure that the student's firstname is valid
+    if not valid_name(student_info["firstname"]):
+        return jsonify({"error": "Student's firstname must be a string!"}), 400
+    
+    # ensure that the student's lastname is valid
+    if not valid_name(student_info["lastname"]):
+        return jsonify({"error": "Student's lastname must be a string!"}), 400
+    
+    # ensure that the student's email is valid
+    if not valid_email(student_info["email"]):
+        return jsonify({"error": "Student's email is not a valid Ashesi email!"}), 400
+    
+    # ensure that the student's date of birth is valid
+    if not valid_dob(student_info["dob"]):
+        return jsonify({"message": f"{student_info['dob']} is not valid."}), 400
+
+    # ensure that the student's major is valid
+    if not valid_major(student_info["major"]):
+        return jsonify({"error": f"{student_info['major']} is not a valid Ashesi major!"}), 400
+    
+    # ensure that the file is an image
+    if "profile_image" in student_info:
+        if not valid_image(student_info["profile_image"]):
+            return jsonify({"error": "Profile image must be an image file!"}), 400
+    
+    # ensure that the provided keys are unique
+    students_collection = USERS_COLLECTION.get()     # retrieve existing user data
+    students_data = list()
+    
+    for document in students_collection:
+        students_data.append(document.to_dict())
+    # key_list, data, object_data
+    unique_data = unique_keys(unique_keys_list, students_data, student_info)
+    if len(unique_data) > 0:
+        return jsonify(unique_data), 400
+    
+    # if all conditions are satisfied, return student info
+    return {"data": student_info}
+
+
+def get_student_by_email(email):
+    
+    # retrieve students data
+    data = USERS_COLLECTION.get()
+    
+    # return appropriate message if no user has been created
+    if not data:
+        return jsonify({"error": "No user has been registered!"}), 404
+    
+    for student in data:
+        student = student.to_dict()
+        if student["email"] == email:
+            return student
+    
+    return jsonify({"error": f"User with email {email} is not registered!"}), 404
+
+
+def valid_post(request):
+    
+    # ensure that the request body is not empty
+    if valid_request_body(request) == False:
+        return jsonify({"error": "Missing post information!"}), 404
+
+    # retrieve request data
+    post_data = json.loads(request.data)
+    
+    # ensure that post_data contains all expected keys
+    attributes = valid_keys(post_data, POST_ATTRIBUTES)
+    if not attributes["is_valid"]:
+        return jsonify(attributes["message"]), 400
+    
+    # ensure that the post data contains at least a description or image
+    if post_data["description"].strip() == "" and post_data["post_image"].strip() == "":
+        return jsonify({"error": "Post data must contain at least a description or image!"}), 400
+    
+    # ensure that the student's email is valid
+    if not valid_email(post_data["email"]):
+        return jsonify({"error": "Student's email is not a valid Ashesi email!"}), 400
+    
+    # ensure that the user with the specified email exists
+    print(get_student_by_email(post_data["email"]))
+    result = get_student_by_email(post_data["email"])
+    if type(result) == tuple:
+        return result
+        
+    # if an image is uploaded, ensure that it is valid
+    if "post_image" in post_data:
+        if not valid_image(post_data["post_image"]):
+            return jsonify({"error": "Post image must be an image file!"}), 400
+    else:
+        post_data["post_image"] = None
+        
+    # add date_created and date_updated to post data
+    
+    current_datetime = datetime.now()
+    # dd/mm/YY H:M:S
+    datetime_formatted = current_datetime.strftime("%d/%m/%Y %H:%M:%S")
+    
+    post_data["date_created"] = datetime_formatted
+    post_data["date_upated"] = datetime_formatted
+    
+    # if all conditions are met, return the data
+    return {"data": post_data}
+
+
+def get_post_by_description(content):
+    """retrieves all posts that had the content specified
+
+    Args:
+        content (str): the content on which posts are to be filtered
+
+    Returns:
+        list: a list of all posts documents (dict) matching the specified content
+    """
+    
+    # retrieve all posts from db
+    posts = POSTS_COLLECTION.get()
+    result = list()
+    
+    # ensure that there are existing posts 
+    if not posts:
+        return jsonify({"error": "No post has been made!"}), 404
+    
+    for post in posts:
+        post = post.to_dict()
+        
+        # check if post contains content specified
+        if content in post["description"]:
+            result.append(post)
+            
+    return result
